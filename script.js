0a989c28617c4114d532482fc9470bed210b64a8
Brandon Curtis <curtiswd@users.noreply.github.com>
2019-08-22 17:13:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-// todo
+$(document).ready(function () {
+	
+	$('#update').on('click', function () {
+
+        var date = new Date($('#datepicker').val());
+        var newDay = date.getDate();
+        var newMonth = date.getMonth();
+        var newYear = date.getFullYear();
+        var newTarget = 'https://apps.aurora.org/cafeteriamenu/calendar.do?method=menu&year=' + newYear + '&month=' + newMonth + '&date=' + newDay + '&location=6';
+        console.log(newTarget);
+
+        $('#menu').attr("src", newTarget);
+        // $('#menu').attr('src', $('#menu').attr('src'));
+    });
+
+    $('#datepicker').datepicker({
+        todayHighlight: 'true'
+
+    });
+});
